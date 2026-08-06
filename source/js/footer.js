6cb1dbe8a4cf695c942414abe846c91b86d9fcb3
shenjunlin00 <108105409+shenjunlin00@users.noreply.github.com>
2026-08-06 18:43:50 +0800

    Add files via upload

---
+++ source/js/footer.js
@@ -0,0 +1,10 @@
+document.write('<!-----------  footer----------->');
+document.write('' +
+    '<center>\n' +
+    '<p style="color:#333;">© 2025-2026 公众号【极客兔】</p>\n' +
+    '</center>\n' +
+    '');
+
+
+
+
